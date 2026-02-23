963fdeb9cd5a67f4a09184a613ab821169f834f6
suraj krishna <surajkrishna.s90@gmail.com>
2026-02-23 20:05:15 +0530

    Fix port for deployment

--- server/index.js
+++ server/index.js
@@ -19,6 +19,8 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/tickets", require("./routes/tickets"));
 
 // Start Server
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });

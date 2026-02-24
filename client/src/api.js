d1e7126b5fe385ce1aea5d22e693e04520c888e1
suraj krishna <surajkrishna.s90@gmail.com>
2026-02-24 17:46:22 +0530

    Fix API base URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://helpdesk-api-wvye.onrender.com"
+  baseURL: "https://helpdesk-api-wyye.onrender.com/api"
 });
 
 API.interceptors.request.use(req => {
